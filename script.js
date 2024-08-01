c1758db0ae308969934e9baa64b91fee55b34d7b
Manuel Meiriño <mmeirino@udesa.edu.ar>
2024-08-01 14:42:53 -0300

    modelos de tarjetas listos

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+$(document).ready(function() {
+    $('.toggle-btn').on('click', function() {
+        var $this = $(this);
+        if ($this.text() === '+') {
+            $this.text('-');
+        } else {
+            $this.text('+');
+        }
+    });
+});
